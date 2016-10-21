a3b98d63346fd99fae1b7e105698a608eaf5d799
shivam <s.v.padaliya@gmail.com>
2016-10-21 13:14:46 -0500

    Start() { Check whether the input number is Whole number or Float }

--- JS/main.js
+++ JS/main.js
@@ -8,3 +8,23 @@ $(document).ready(function()
 
     $('#start').on('click',function()
     {
+        //The number inputed by the user is a whole number.
+        var num = $("#number-input").val();
+
+        if((parseFloat(num) === parseInt(num)))
+        {
+            console.log('The input number');
+            console.log(num);
+            countUp(num);
+        }
+
+        else
+        {
+            //The number inputed by the user is not a whole number.
+            //Display the error alert!
+
+            alert("Opps!! You entered the wrong number. Please enter whole number.!!");
+        }
+    });
+
+}
